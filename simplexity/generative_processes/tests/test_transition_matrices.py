ca75e116f13a5e62824fc0a1d03f014bfde562e9
Eric Alt <ericallenalt@gmail.com>
2025-02-07 21:55:54 +0000

    Clean up transition matrices file

--- simplexity/generative_processes/tests/test_transition_matrices.py
+++ simplexity/generative_processes/tests/test_transition_matrices.py
@@ -1,3 +1,5 @@
+import jax.numpy as jnp
+
 from simplexity.generative_processes.transition_matrices import (
     days_of_week,
     fanizza,
@@ -11,6 +13,10 @@ from simplexity.generative_processes.transition_matrices import (
 def test_post_quantum():
     transition_matrices = post_quantum()
     assert transition_matrices.shape == (3, 3, 3)
+    # Verify that transition_matrix[0] + transition_matrix[1] + transition_matrix[2] has largest abs eigenvalue = 1
+    transition_matrix_sum_normalized = transition_matrices.sum(axis=0)
+    transition_matrix_sum_max_eigval = jnp.abs(jnp.linalg.eigvals(transition_matrix_sum_normalized)).max()
+    assert jnp.isclose(transition_matrix_sum_max_eigval, 1, atol=1e-10), "Largest absolute eigenvalue is not 1"
 
 
 def test_days_of_week():
@@ -26,6 +32,10 @@ def test_tom_quantum():
 def test_fanizza():
     transition_matrices = fanizza(alpha=2000, lamb=0.49)
     assert transition_matrices.shape == (2, 4, 4)
+    tau = jnp.ones(4)
+    assert jnp.allclose(transition_matrices[0] @ tau + transition_matrices[1] @ tau, tau), (
+        "Stochasticity condition not met"
+    )
 
 
 def test_rrxor():
