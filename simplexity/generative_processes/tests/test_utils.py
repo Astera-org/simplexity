7f21cb535432fb2bbb60f2633dc20467779f68e0
Eric Alt <ericallenalt@gmail.com>
2025-02-10 18:26:53 +0000

    Add unit tests for util functions

--- simplexity/generative_processes/tests/test_utils.py
+++ simplexity/generative_processes/tests/test_utils.py
@@ -1,7 +1,7 @@
 import jax
 import jax.numpy as jnp
 
-from simplexity.generative_processes.utils import log_matmul
+from simplexity.generative_processes.utils import log_matmul, normalize_simplex, stationary_distribution
 
 
 def test_log_matmul():
@@ -9,3 +9,21 @@ def test_log_matmul():
     A = jax.random.uniform(key_a, (3, 4))
     B = jax.random.uniform(key_b, (4, 5))
     assert jnp.allclose(log_matmul(jnp.log(A), jnp.log(B)), jnp.log(A @ B))
+
+
+def test_normalize_simplex():
+    simplex = jnp.array([1, 3, 4])
+    expected = jnp.array([0.125, 0.375, 0.5])
+    assert jnp.allclose(normalize_simplex(simplex), expected)
+    assert jnp.allclose(normalize_simplex(-simplex), expected)
+
+
+def test_stationary_distribution():
+    transition_matrix = jnp.array([
+        [0, 0, 0, 1], 
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+    ])
+    expected = jnp.array([0.25, 0.25, 0.25, 0.25])
+    assert jnp.allclose(stationary_distribution(transition_matrix), expected)
