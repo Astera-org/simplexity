8fb5a59425d1353d0a30c39956824a4e52e27359
Eric Alt <ericallenalt@gmail.com>
2025-03-05 19:41:32 +0000

    Refactor RNN so __call__ just calls layers
    Indexing layers breaks down when penzai is used to modify the layers

--- simplexity/predictive_models/rnn.py
+++ simplexity/predictive_models/rnn.py
@@ -6,28 +6,44 @@ import jax.numpy as jnp
 from simplexity.predictive_models.predictive_model import PredictiveModel
 
 
-class RNN(PredictiveModel):
+class GRUFn(eqx.Module):
     """A simple RNN model."""
 
     hidden_size: int = eqx.static_field()
+    cell: eqx.nn.GRUCell
+
+    def __init__(self, in_size: int, hidden_size: int, key: chex.PRNGKey):
+        self.hidden_size = hidden_size
+        self.cell = eqx.nn.GRUCell(in_size, hidden_size, key=key)
+
+    def __call__(self, xs: jax.Array) -> jax.Array:
+        """Forward pass of the RNN."""
+
+        def process_element(carry, x):
+            return self.cell(x, carry), None
+
+        hidden = jnp.zeros(self.hidden_size)
+        out, _ = jax.lax.scan(process_element, hidden, xs)
+        return out
+
+
+class RNN(PredictiveModel):
+    """A simple RNN model."""
+
     layers: eqx.nn.Sequential
 
     def __init__(self, in_size: int, out_size: int, hidden_size: int, *, key: chex.PRNGKey):
         cell_key, linear_key = jax.random.split(key)
-        self.hidden_size = hidden_size
+
+        gru_fn = GRUFn(in_size, hidden_size, key=cell_key)
+
         self.layers = eqx.nn.Sequential(
             [
-                eqx.nn.GRUCell(in_size, hidden_size, key=cell_key),
+                eqx.nn.Lambda(gru_fn),
                 eqx.nn.Linear(hidden_size, out_size, use_bias=False, key=linear_key),
             ]
         )
 
     def __call__(self, xs: jax.Array) -> jax.Array:
         """Forward pass of the RNN."""
-        hidden = jnp.zeros(self.hidden_size)
-
-        def f(carry, x):
-            return self.layers[0](x, carry), None
-
-        out, _ = jax.lax.scan(f, hidden, xs)
-        return self.layers[1](out)
+        return self.layers(xs)
