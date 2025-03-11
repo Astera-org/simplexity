bc04bfe087e575119d5f0fa6694f90e5cebca736
Eric Alt <ericallenalt@gmail.com>
2025-03-11 00:54:40 +0000

    Create local persister

---
+++ simplexity/persistence/model_persister.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+
+import equinox as eqx
+
+from simplexity.predictive_models.predictive_model import PredictiveModel
+
+
+class ModelPersister(eqx.Module):
+    """Persists a model to a file."""
+
+    @abstractmethod
+    def save_weights(self, model: PredictiveModel, name: str) -> None:
+        """Saves a model."""
+        ...
+
+    @abstractmethod
+    def load_weights(self, model: PredictiveModel, name: str) -> PredictiveModel:
+        """Load weights into an existing model instance."""
+        ...
---
+++ tests/persistence/test_local_persister.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import chex
+import pytest
+
+from simplexity.persistence.local_persister import LocalPersister
+from simplexity.predictive_models.rnn import RNN
+
+
+def get_model(seed: int) -> RNN:
+    return RNN(in_size=1, out_size=2, hidden_sizes=[3, 3], seed=seed)
+
+
+def test_local_persister(tmp_path: Path):
+    base_dir_str = str(tmp_path)
+    persister = LocalPersister(base_dir_str)
+    assert persister.base_dir == base_dir_str
+
+    model = get_model(0)
+    persister.save_weights(model, "test_model")
+    assert (tmp_path / "test_model.eqx").exists()
+
+    new_model = get_model(1)
+    with pytest.raises(AssertionError):
+        chex.assert_trees_all_close(new_model, model)
+    loaded_model = persister.load_weights(new_model, "test_model")
+    chex.assert_trees_all_equal(loaded_model, model)
