58b28147ba0b01b21dd0ad875d792e14e5aa799a
Eric Alt <ericallenalt@gmail.com>
2025-03-12 20:00:48 +0000

    Construct RNN from builder

--- simplexity/configs/predictive_model/config.py
+++ simplexity/configs/predictive_model/config.py
@@ -6,16 +6,17 @@ from typing import Literal
 class ModelInstanceConfig:
     """Configuration for the model instance."""
 
-    _target_: Literal["simplexity.predictive_models.rnn.RNN"]
+    _target_: Literal["simplexity.predictive_models.rnn.build_rnn"]
 
 
 @dataclass
 class RNNConfig(ModelInstanceConfig):
     """Configuration for RNN model."""
 
-    _target_: Literal["simplexity.predictive_models.rnn.RNN"]
+    _target_: Literal["simplexity.predictive_models.rnn.build_rnn"]
     in_size: int
-    hidden_sizes: list[int]
+    hidden_size: int
+    num_layers: int
     out_size: int
     seed: int
 
