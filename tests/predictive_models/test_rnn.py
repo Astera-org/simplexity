ef1e88966c64fb3e522706ead6701ed27291fa71
Eric Alt <ericallenalt@gmail.com>
2025-03-14 19:57:32 +0000

    Change sizes of test rnn

--- tests/predictive_models/test_rnn.py
+++ tests/predictive_models/test_rnn.py
@@ -5,11 +5,11 @@ from simplexity.predictive_models.rnn import RNN
 
 def test_rnn():
     key = jax.random.PRNGKey(0)
-    vocab_size = 2
-    model = RNN(in_size=vocab_size, out_size=vocab_size, hidden_sizes=[4, 4], key=key)
+    in_size = 8
+    out_size = 2
+    model = RNN(in_size=in_size, out_size=out_size, hidden_sizes=[6, 4], key=key)
 
-    sequence_len = 2
-    xs = jax.random.randint(key, (sequence_len,), 0, vocab_size)
-    one_hot_xs = jax.nn.one_hot(xs, vocab_size)
-    ys = model(one_hot_xs)
-    assert ys.shape == (sequence_len, vocab_size)
+    sequence_len = 16
+    xs = jax.random.randint(key, (sequence_len, in_size), 0, 2)
+    ys = model(xs)
+    assert ys.shape == (sequence_len, out_size)
