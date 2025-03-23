3bf43123ef5ef4da77da55c2509993d64a384b6a
Eric Alt <ericallenalt@gmail.com>
2025-03-23 01:01:40 +0000

    Fix Mess3 matrices

--- simplexity/generative_processes/transition_matrices.py
+++ simplexity/generative_processes/transition_matrices.py
@@ -92,19 +92,19 @@ def mess3(x: float, a: float) -> jax.Array:
     return jnp.array(
         [
             [
-                [ay, ax, ax],
-                [bx, by, bx],
-                [bx, bx, by],
+                [ay, bx, bx],
+                [ax, by, bx],
+                [ax, bx, by],
             ],
             [
-                [by, bx, bx],
-                [ax, ay, ax],
-                [bx, bx, by],
+                [by, ax, bx],
+                [bx, ay, bx],
+                [bx, ax, by],
             ],
             [
-                [by, bx, bx],
-                [bx, by, bx],
-                [ax, ax, ay],
+                [by, bx, ax],
+                [bx, by, ax],
+                [bx, bx, ay],
             ],
         ]
     )
