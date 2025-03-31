c29917411c7887eb1bbfe1238591b8cdedfb2d35
Eric Alt <ericallenalt@gmail.com>
2025-03-31 07:46:32 +0000

    Separate CONFIG from PARAMS

--- tests/test_penzai_utils.py
+++ tests/test_penzai_utils.py
@@ -10,36 +10,36 @@ from simplexity.penzai_utils import (
     get_parameter_tree,
 )
 
-PARAMS = {
-    "embedding_dim": 2,
-    "num_decoder_blocks": 3,
-    "num_kv_heads": 5,
-    "projection_dim": 7,
-    "query_head_multiplier": 11,
-    "mlp_hidden_dim": 13,
-    "vocab_size": 17,
-}
+CONFIG = LlamalikeTransformerConfig(
+    embedding_dim=2,
+    num_decoder_blocks=3,
+    num_kv_heads=5,
+    projection_dim=7,
+    query_head_multiplier=11,
+    mlp_hidden_dim=13,
+    vocab_size=17,
+    mlp_variant="geglu_approx",
+    tie_embedder_and_logits=False,
+)
 
-PARAMS["embeddings"] = PARAMS["vocab_size"] * PARAMS["embedding_dim"]
-PARAMS["layer_norm"] = PARAMS["embedding_dim"]
-PARAMS["queries"] = (
-    PARAMS["embedding_dim"] * PARAMS["num_kv_heads"] * PARAMS["query_head_multiplier"] * PARAMS["projection_dim"]
-)
-PARAMS["keys"] = PARAMS["embedding_dim"] * PARAMS["num_kv_heads"] * PARAMS["projection_dim"]
-PARAMS["values"] = PARAMS["embedding_dim"] * PARAMS["num_kv_heads"] * PARAMS["projection_dim"]
-PARAMS["outputs"] = (
-    PARAMS["embedding_dim"] * PARAMS["num_kv_heads"] * PARAMS["query_head_multiplier"] * PARAMS["projection_dim"]
-)
+PARAMS = {
+    "embeddings": CONFIG.vocab_size * CONFIG.embedding_dim,
+    "layer_norm": CONFIG.embedding_dim,
+    "queries": CONFIG.embedding_dim * CONFIG.num_kv_heads * CONFIG.query_head_multiplier * CONFIG.projection_dim,
+    "keys": CONFIG.embedding_dim * CONFIG.num_kv_heads * CONFIG.projection_dim,
+    "values": CONFIG.embedding_dim * CONFIG.num_kv_heads * CONFIG.projection_dim,
+    "outputs": CONFIG.embedding_dim * CONFIG.num_kv_heads * CONFIG.query_head_multiplier * CONFIG.projection_dim,
+    "mlp_gate": CONFIG.embedding_dim * CONFIG.mlp_hidden_dim,
+    "mlp_value": CONFIG.embedding_dim * CONFIG.mlp_hidden_dim,
+    "mlp_output": CONFIG.mlp_hidden_dim * CONFIG.embedding_dim,
+    "lm_head": CONFIG.embedding_dim * CONFIG.vocab_size,
+}
 PARAMS["attention"] = PARAMS["queries"] + PARAMS["keys"] + PARAMS["values"] + PARAMS["outputs"]
-PARAMS["mlp_gate"] = PARAMS["embedding_dim"] * PARAMS["mlp_hidden_dim"]
-PARAMS["mlp_value"] = PARAMS["embedding_dim"] * PARAMS["mlp_hidden_dim"]
-PARAMS["mlp_output"] = PARAMS["mlp_hidden_dim"] * PARAMS["embedding_dim"]
 PARAMS["mlp"] = PARAMS["mlp_gate"] + PARAMS["mlp_value"] + PARAMS["mlp_output"]
 PARAMS["decoder_block"] = PARAMS["layer_norm"] + PARAMS["attention"] + PARAMS["layer_norm"] + PARAMS["mlp"]
-PARAMS["lm_head"] = PARAMS["embedding_dim"] * PARAMS["vocab_size"]
 PARAMS["total"] = (
     PARAMS["embeddings"]
-    + PARAMS["decoder_block"] * PARAMS["num_decoder_blocks"]
+    + PARAMS["decoder_block"] * CONFIG.num_decoder_blocks
     + PARAMS["layer_norm"]
     + PARAMS["lm_head"]
 )
@@ -47,19 +47,8 @@ PARAMS["total"] = (
 
 @pytest.fixture
 def transformer():
-    config = LlamalikeTransformerConfig(
-        num_kv_heads=PARAMS["num_kv_heads"],
-        query_head_multiplier=PARAMS["query_head_multiplier"],
-        embedding_dim=PARAMS["embedding_dim"],
-        projection_dim=PARAMS["projection_dim"],
-        mlp_hidden_dim=PARAMS["mlp_hidden_dim"],
-        num_decoder_blocks=PARAMS["num_decoder_blocks"],
-        vocab_size=PARAMS["vocab_size"],
-        mlp_variant="geglu_approx",
-        tie_embedder_and_logits=False,
-    )
     base_rng = jax.random.PRNGKey(0)
-    return build_llamalike_transformer(config, init_base_rng=base_rng)
+    return build_llamalike_transformer(CONFIG, init_base_rng=base_rng)
 
 
 def test_get_parameter_count(transformer):
@@ -75,7 +64,7 @@ def test_get_parameter_tree(transformer):
         children=[],
     )
 
-    transformer_block = ParameterTree(
+    decoder_block = ParameterTree(
         name="TransformerBlock",
         parameters=PARAMS["decoder_block"],
         children=[
@@ -159,7 +148,7 @@ def test_get_parameter_tree(transformer):
                 children=[],
             )
         ]
-        + [transformer_block] * PARAMS["num_decoder_blocks"]
+        + [decoder_block] * CONFIG.num_decoder_blocks
         + [layer_norm]
         + [
             ParameterTree(
@@ -219,7 +208,7 @@ def test_get_parameter_list(transformer):
             parameters=PARAMS["embeddings"],
         ),
     ]
-    expected += decoder_block * PARAMS["num_decoder_blocks"]
+    expected += decoder_block * CONFIG.num_decoder_blocks
     expected += [
         NamedParameters(
             name="TransformerLM.Sequential.RMSLayerNorm.Linear",
