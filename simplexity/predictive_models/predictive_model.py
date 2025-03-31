5e44ff4aca2b2a573dd46437e36daacf61791c49
Eric Alt <ericallenalt@gmail.com>
2025-03-31 20:27:17 +0000

    Remove in and out size from PredictiveModel protocol

--- simplexity/predictive_models/predictive_model.py
+++ simplexity/predictive_models/predictive_model.py
@@ -7,9 +7,6 @@ import jax
 class PredictiveModel(Protocol):
     """A predictive model that takes observations and returns a logit distribution over observations."""
 
-    in_size: int
-    out_size: int
-
     def __call__(self, x: jax.Array, /) -> jax.Array:
         """Predict the next state given the current state."""
         ...
--- simplexity/training/train.py
+++ simplexity/training/train.py
@@ -66,7 +66,7 @@ def training_step(
     batch_keys = jax.random.split(key, attrs.batch_size)
     gen_process_states, obs = attrs.gen_process.generate(state.gen_process_states, batch_keys, attrs.sequence_len)
     state = dataclasses.replace(state, gen_process_states=gen_process_states)
-    one_hot_obs = jax.nn.one_hot(obs, state.model.out_size)
+    one_hot_obs = jax.nn.one_hot(obs, attrs.gen_process.vocab_size)
     x = one_hot_obs[:, :-1, :]
     y = one_hot_obs[:, 1:, :].squeeze()
     return update(state, x, y, attrs.opt_update)
