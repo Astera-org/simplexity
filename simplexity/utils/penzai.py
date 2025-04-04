efbea30fba32c960ed10a09e6a2b67737e4f40e6
Eric Alt <ericallenalt@gmail.com>
2025-04-04 20:43:04 +0000

    Merge origin/main

--- simplexity/utils/penzai.py
+++ simplexity/utils/penzai.py
@@ -1,117 +1,39 @@
-import dataclasses
-import itertools
-from dataclasses import dataclass, field
-from typing import Any
+from dataclasses import dataclass
 
+from penzai import pz
 from penzai.core.named_axes import NamedArray
 from penzai.core.struct import Struct
-from penzai.core.variables import Parameter
-from penzai.models.transformer.variants.llamalike_common import LlamalikeTransformerConfig
-
-
-def calculate_llamalike_transformer_parameter_count(config: LlamalikeTransformerConfig) -> int:
-    """Calculate the number of parameters in a Penzai transformer."""
-    return (
-        1  # final layer norm
-        + 2 * config.vocab_size  # embedding table and LM head weights
-        + (  # decoder block
-            2  # layer norms before attention and MLP
-            + 2 * config.num_kv_heads * (1 + config.query_head_multiplier) * config.projection_dim  # QKV+output weights
-            + 3 * config.mlp_hidden_dim  # gating, value, and output weights
-        )
-        * config.num_decoder_blocks
-    ) * config.embedding_dim
-
-
-def get_parameter_count(x: Struct | list[Any] | Parameter) -> int:
-    """Count the total number of parameters in a Penzai structure."""
-    if isinstance(x, Struct):
-        children = x.tree_flatten()[0]
-        if not isinstance(children, Struct | list | Parameter):
-            return 0
-        return get_parameter_count(children)
-
-    if isinstance(x, list):
-        return sum(get_parameter_count(i) for i in x if isinstance(i, Struct | list | Parameter))
-
-    assert isinstance(x, Parameter)
-    named_array: NamedArray = x.value
-    return named_array.data_array.size
 
 
 @dataclass
-class ParameterTree:
-    """A tree of parameters."""
-
-    name: str = ""
-    parameters: int = 0
-    children: list["ParameterTree"] = field(default_factory=list)
-
-
-def get_parameter_tree(x: Struct | list[Any] | Parameter) -> ParameterTree:
-    """Construct a parameter tree from a Penzai structure."""
-    if isinstance(x, Struct):
-        children = x.tree_flatten()[0]
-        if not isinstance(children, Struct | list | Parameter):
-            return ParameterTree()
-        subtree = get_parameter_tree(children)
-        if subtree == ParameterTree():
-            return ParameterTree()
-        name = x.__class__.__name__
-        if subtree.name not in (list.__name__, Parameter.__name__):
-            name = f"{name}.{subtree.name}"
-        return ParameterTree(
-            name=name,
-            parameters=subtree.parameters,
-            children=subtree.children,
-        )
-
-    if isinstance(x, list):
-        children = [get_parameter_tree(i) for i in x if isinstance(i, Struct | list | Parameter)]
-        children = [child for child in children if child != ParameterTree()]
-        parameters = sum(child.parameters for child in children)
-        if parameters == 0:
-            return ParameterTree()
-        name = x.__class__.__name__
-        if len(children) == 1:
-            child_name = children[0].name
-            if name == list.__name__:
-                name = child_name
-            elif child_name not in (list.__name__, Parameter.__name__):
-                name = f"{name}.{child_name}"
-            children = children[0].children
-        return ParameterTree(
-            name=name,
-            parameters=parameters,
-            children=children,
-        )
-
-    assert isinstance(x, Parameter)
-    named_array: NamedArray = x.value
-    parameters = named_array.data_array.size
-    return ParameterTree(
-        name=Parameter.__name__,
-        parameters=parameters,
-        children=[],
-    )
-
-
-@dataclass
-class NamedParameters:
-    """A named parameter."""
+class ParamCountNode:
+    """A node in the parameter count tree."""
 
     name: str
-    parameters: int
+    param_count: int
+    children: list["ParamCountNode"]
 
 
-def get_parameter_list(parameter_tree: ParameterTree) -> list[NamedParameters]:
-    """Get a list of named parameters from a parameter tree."""
-    if not parameter_tree.children:
-        return [NamedParameters(name=parameter_tree.name, parameters=parameter_tree.parameters)]
-
-    parameter_list = list(itertools.chain(*map(get_parameter_list, parameter_tree.children)))
-
-    def modify_name(named_param: NamedParameters) -> NamedParameters:
-        return dataclasses.replace(named_param, name=f"{parameter_tree.name}.{named_param.name}")
-
-    return list(map(modify_name, parameter_list))
+def get_parameter_count_tree(struct: Struct) -> ParamCountNode:
+    """Get a tree of the parameter counts for a struct."""
+    _, params = pz.unbind_params(struct)
+    root = ParamCountNode(name="", param_count=0, children=[])
+    for param in params:
+        named_array: NamedArray = param.value
+        param_count = named_array.data_array.size
+        label = str(param.label)
+        label_parts = label.split("/")
+        current = root
+        for name in label_parts:
+            children_names = [i.name for i in current.children]
+            try:
+                i = children_names.index(name)
+            except ValueError:
+                current.children.append(ParamCountNode(name=name, param_count=0, children=[]))
+                i = len(current.children) - 1
+            current.param_count += param_count
+            current = current.children[i]
+        current.param_count += param_count
+    if len(root.children) == 1:
+        return root.children[0]
+    return root
--- tests/utils/test_penzai.py
+++ tests/utils/test_penzai.py
@@ -1,227 +1,157 @@
+import dataclasses
+
 import jax
-import pytest
 from penzai.models.transformer.variants.llamalike_common import LlamalikeTransformerConfig, build_llamalike_transformer
 
-from simplexity.utils.penzai import (
-    NamedParameters,
-    ParameterTree,
-    calculate_llamalike_transformer_parameter_count,
-    get_parameter_count,
-    get_parameter_list,
-    get_parameter_tree,
-)
-
-CONFIG = LlamalikeTransformerConfig(
-    embedding_dim=2,
-    num_decoder_blocks=3,
-    num_kv_heads=5,
-    projection_dim=7,
-    query_head_multiplier=11,
-    mlp_hidden_dim=13,
-    vocab_size=17,
-    mlp_variant="geglu_approx",
-    tie_embedder_and_logits=False,
-)
-
-PARAMS = {
-    "embeddings": CONFIG.vocab_size * CONFIG.embedding_dim,
-    "layer_norm": CONFIG.embedding_dim,
-    "queries": CONFIG.embedding_dim * CONFIG.num_kv_heads * CONFIG.query_head_multiplier * CONFIG.projection_dim,
-    "keys": CONFIG.embedding_dim * CONFIG.num_kv_heads * CONFIG.projection_dim,
-    "values": CONFIG.embedding_dim * CONFIG.num_kv_heads * CONFIG.projection_dim,
-    "outputs": CONFIG.embedding_dim * CONFIG.num_kv_heads * CONFIG.query_head_multiplier * CONFIG.projection_dim,
-    "mlp_gate": CONFIG.embedding_dim * CONFIG.mlp_hidden_dim,
-    "mlp_value": CONFIG.embedding_dim * CONFIG.mlp_hidden_dim,
-    "mlp_output": CONFIG.mlp_hidden_dim * CONFIG.embedding_dim,
-    "lm_head": CONFIG.embedding_dim * CONFIG.vocab_size,
-}
-PARAMS["attention"] = PARAMS["queries"] + PARAMS["keys"] + PARAMS["values"] + PARAMS["outputs"]
-PARAMS["mlp"] = PARAMS["mlp_gate"] + PARAMS["mlp_value"] + PARAMS["mlp_output"]
-PARAMS["decoder_block"] = PARAMS["layer_norm"] + PARAMS["attention"] + PARAMS["layer_norm"] + PARAMS["mlp"]
-PARAMS["total"] = (
-    PARAMS["embeddings"]
-    + PARAMS["decoder_block"] * CONFIG.num_decoder_blocks
-    + PARAMS["layer_norm"]
-    + PARAMS["lm_head"]
-)
+from simplexity.utils.penzai import ParamCountNode, get_parameter_count_tree
 
 
-@pytest.fixture
-def transformer():
+def test_get_parameter_count_tree():
+    config = LlamalikeTransformerConfig(
+        embedding_dim=2,
+        num_decoder_blocks=3,
+        num_kv_heads=5,
+        projection_dim=7,
+        query_head_multiplier=11,
+        mlp_hidden_dim=13,
+        vocab_size=17,
+        mlp_variant="geglu_approx",
+        tie_embedder_and_logits=False,
+    )
     base_rng = jax.random.PRNGKey(0)
-    return build_llamalike_transformer(CONFIG, init_base_rng=base_rng)
+    transformer = build_llamalike_transformer(config, init_base_rng=base_rng)
+    actual = get_parameter_count_tree(transformer)
 
-
-def test_get_parameter_count_from_config():
-    assert calculate_llamalike_transformer_parameter_count(CONFIG) == PARAMS["total"]
-
-
-def test_get_parameter_count(transformer):
-    assert get_parameter_count(transformer) == PARAMS["total"]
-
-
-def test_get_parameter_tree(transformer):
-    actual = get_parameter_tree(transformer)
-
-    layer_norm = ParameterTree(
-        name="RMSLayerNorm.Linear",
-        parameters=PARAMS["layer_norm"],
-        children=[],
+    param_counts = {
+        "embeddings": config.vocab_size * config.embedding_dim,
+        "layer_norm": config.embedding_dim,
+        "queries": config.embedding_dim * config.num_kv_heads * config.query_head_multiplier * config.projection_dim,
+        "keys": config.embedding_dim * config.num_kv_heads * config.projection_dim,
+        "values": config.embedding_dim * config.num_kv_heads * config.projection_dim,
+        "outputs": config.embedding_dim * config.num_kv_heads * config.query_head_multiplier * config.projection_dim,
+        "mlp_gate": config.embedding_dim * config.mlp_hidden_dim,
+        "mlp_value": config.embedding_dim * config.mlp_hidden_dim,
+        "mlp_output": config.mlp_hidden_dim * config.embedding_dim,
+        "lm_head": config.embedding_dim * config.vocab_size,
+    }
+    param_counts["attention"] = (
+        param_counts["queries"] + param_counts["keys"] + param_counts["values"] + param_counts["outputs"]
+    )
+    param_counts["mlp"] = param_counts["mlp_gate"] + param_counts["mlp_value"] + param_counts["mlp_output"]
+    param_counts["decoder_block"] = (
+        param_counts["layer_norm"] + param_counts["attention"] + param_counts["layer_norm"] + param_counts["mlp"]
+    )
+    param_counts["transformer"] = (
+        param_counts["embeddings"]
+        + param_counts["decoder_block"] * config.num_decoder_blocks
+        + param_counts["layer_norm"]
+        + param_counts["lm_head"]
     )
 
-    decoder_block = ParameterTree(
-        name="TransformerBlock",
-        parameters=PARAMS["decoder_block"],
+    decoder_block = ParamCountNode(
+        name="block_{block_index}",
+        param_count=param_counts["decoder_block"],
         children=[
-            ParameterTree(
-                name="Residual.Sequential",
-                parameters=PARAMS["layer_norm"] + PARAMS["attention"],
+            ParamCountNode(
+                name="pre_attention_norm",
+                param_count=param_counts["layer_norm"],
                 children=[
-                    layer_norm,
-                    ParameterTree(
-                        name="Attention",
-                        parameters=PARAMS["attention"],
-                        children=[
-                            ParameterTree(
-                                name="Sequential.Linear",
-                                parameters=PARAMS["queries"],
-                                children=[],
-                            ),
-                            ParameterTree(
-                                name="Sequential.Linear",
-                                parameters=PARAMS["keys"],
-                                children=[],
-                            ),
-                            ParameterTree(
-                                name="Sequential.Linear",
-                                parameters=PARAMS["values"],
-                                children=[],
-                            ),
-                            ParameterTree(
-                                name="Sequential.Linear",
-                                parameters=PARAMS["outputs"],
-                                children=[],
-                            ),
-                        ],
+                    ParamCountNode(
+                        name="scale.weights",
+                        param_count=param_counts["layer_norm"],
+                        children=[],
+                    )
+                ],
+            ),
+            ParamCountNode(
+                name="attention",
+                param_count=param_counts["attention"],
+                children=[
+                    ParamCountNode(
+                        name="query.weights",
+                        param_count=param_counts["queries"],
+                        children=[],
+                    ),
+                    ParamCountNode(
+                        name="key.weights",
+                        param_count=param_counts["keys"],
+                        children=[],
+                    ),
+                    ParamCountNode(
+                        name="value.weights",
+                        param_count=param_counts["values"],
+                        children=[],
+                    ),
+                    ParamCountNode(
+                        name="output.weights",
+                        param_count=param_counts["outputs"],
+                        children=[],
                     ),
                 ],
             ),
-            ParameterTree(
-                name="Residual.Sequential",
-                parameters=PARAMS["layer_norm"] + PARAMS["mlp"],
+            ParamCountNode(
+                name="pre_ffw_norm",
+                param_count=param_counts["layer_norm"],
                 children=[
-                    layer_norm,
-                    ParameterTree(
-                        name="TransformerFeedForward",
-                        parameters=PARAMS["mlp"],
-                        children=[
-                            ParameterTree(
-                                name="BranchAndMultiplyTogether",
-                                parameters=PARAMS["mlp_gate"] + PARAMS["mlp_value"],
-                                children=[
-                                    ParameterTree(
-                                        name="NamedGroup.Linear",
-                                        parameters=PARAMS["mlp_gate"],
-                                        children=[],
-                                    ),
-                                    ParameterTree(
-                                        name="Linear",
-                                        parameters=PARAMS["mlp_value"],
-                                        children=[],
-                                    ),
-                                ],
-                            ),
-                            ParameterTree(
-                                name="Linear",
-                                parameters=PARAMS["mlp_output"],
-                                children=[],
-                            ),
-                        ],
+                    ParamCountNode(
+                        name="scale.weights",
+                        param_count=param_counts["layer_norm"],
+                        children=[],
+                    )
+                ],
+            ),
+            ParamCountNode(
+                name="mlp",
+                param_count=param_counts["mlp"],
+                children=[
+                    ParamCountNode(
+                        name="gating_linear.weights",
+                        param_count=param_counts["mlp_gate"],
+                        children=[],
+                    ),
+                    ParamCountNode(
+                        name="value_linear.weights",
+                        param_count=param_counts["mlp_value"],
+                        children=[],
+                    ),
+                    ParamCountNode(
+                        name="out_linear.weights",
+                        param_count=param_counts["mlp_output"],
+                        children=[],
                     ),
                 ],
             ),
         ],
     )
 
-    expected = ParameterTree(
-        name="TransformerLM.Sequential",
-        parameters=PARAMS["total"],
+    expected = ParamCountNode(
+        name="transformer",
+        param_count=param_counts["transformer"],
         children=[
-            ParameterTree(
-                name="EmbeddingLookup.EmbeddingTable",
-                parameters=PARAMS["embeddings"],
+            ParamCountNode(
+                name="embedder.embeddings",
+                param_count=param_counts["embeddings"],
                 children=[],
-            )
+            ),
         ]
-        + [decoder_block] * CONFIG.num_decoder_blocks
-        + [layer_norm]
+        + [dataclasses.replace(decoder_block, name=f"block_{i}") for i in range(config.num_decoder_blocks)]
         + [
-            ParameterTree(
-                name="Linear",
-                parameters=PARAMS["lm_head"],
+            ParamCountNode(
+                name="final_norm",
+                param_count=param_counts["layer_norm"],
+                children=[
+                    ParamCountNode(
+                        name="scale.weights",
+                        param_count=param_counts["layer_norm"],
+                        children=[],
+                    ),
+                ],
+            ),
+            ParamCountNode(
+                name="lm_head.weights",
+                param_count=param_counts["lm_head"],
                 children=[],
-            )
+            ),
         ],
     )
-    assert actual == expected
-
-
-def test_get_parameter_list(transformer):
-    tree = get_parameter_tree(transformer)
-    actual = get_parameter_list(tree)
-    decoder_block = [
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.RMSLayerNorm.Linear",
-            parameters=PARAMS["layer_norm"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.Attention.Sequential.Linear",
-            parameters=PARAMS["queries"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.Attention.Sequential.Linear",
-            parameters=PARAMS["keys"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.Attention.Sequential.Linear",
-            parameters=PARAMS["values"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.Attention.Sequential.Linear",
-            parameters=PARAMS["outputs"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.RMSLayerNorm.Linear",
-            parameters=PARAMS["layer_norm"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.TransformerFeedForward.BranchAndMultiplyTogether.NamedGroup.Linear",
-            parameters=PARAMS["mlp_gate"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.TransformerFeedForward.BranchAndMultiplyTogether.Linear",
-            parameters=PARAMS["mlp_value"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.TransformerBlock.Residual.Sequential.TransformerFeedForward.Linear",
-            parameters=PARAMS["mlp_output"],
-        ),
-    ]
-    expected = [
-        NamedParameters(
-            name="TransformerLM.Sequential.EmbeddingLookup.EmbeddingTable",
-            parameters=PARAMS["embeddings"],
-        ),
-    ]
-    expected += decoder_block * CONFIG.num_decoder_blocks
-    expected += [
-        NamedParameters(
-            name="TransformerLM.Sequential.RMSLayerNorm.Linear",
-            parameters=PARAMS["layer_norm"],
-        ),
-        NamedParameters(
-            name="TransformerLM.Sequential.Linear",
-            parameters=PARAMS["lm_head"],
-        ),
-    ]
     assert actual == expected
