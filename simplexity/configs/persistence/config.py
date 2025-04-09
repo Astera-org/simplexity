b398142f2cf70b3cfde0bf5dec7cd7a71dc2ecb4
Eric Alt <ericallenalt@gmail.com>
2025-04-09 08:58:40 +0000

    Fix persistence configs

--- simplexity/configs/persistence/config.py
+++ simplexity/configs/persistence/config.py
@@ -7,28 +7,38 @@ class PersistenceInstanceConfig:
     """Configuration for the persistence instance."""
 
     _target_: Literal[
-        "simplexity.persistence.local_persister.LocalPersister",
+        "simplexity.persistence.local_equinox_persister.LocalEquinoxPersister",
+        "simplexity.persistence.local_penzai_persister.LocalPenzaiPersister",
         "simplexity.persistence.s3_persister.S3Persister.from_client_args",
     ]
 
 
 @dataclass
-class LocalPersisterConfig(PersistenceInstanceConfig):
-    """Configuration for local persister."""
+class LocalEquinoxPersisterConfig(PersistenceInstanceConfig):
+    """Configuration for local equinox persister."""
 
-    base_dir: str
+    directory: str
+    filename: str
+
+
+@dataclass
+class LocalPenzaiPersisterConfig(PersistenceInstanceConfig):
+    """Configuration for local penzai persister."""
+
+    directory: str
 
 
 @dataclass
 class S3PersisterConfig(PersistenceInstanceConfig):
     """Configuration for S3 persister."""
 
-    config_file: str
+    filename: str
+    model_framework: str
 
 
 @dataclass
 class Config:
     """Base configuration for persistence."""
 
-    name: Literal["local_persister", "s3_persister"]
+    name: Literal["local_equinox_persister", "local_penzai_persister", "s3_persister"]
     instance: PersistenceInstanceConfig
