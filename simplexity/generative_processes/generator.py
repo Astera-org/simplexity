833785921f525c500e4ffc182cc1e7949f537a8d
Eric Alt <ericallenalt@gmail.com>
2025-05-14 21:04:59 +0000

    Add bos and eos tokens

--- simplexity/generative_processes/generator.py
+++ simplexity/generative_processes/generator.py
@@ -1,5 +1,6 @@
 import equinox as eqx
 import jax
+import jax.numpy as jnp
 
 from simplexity.generative_processes.generative_process import GenerativeProcess
 
@@ -11,10 +12,16 @@ def generate_data_batch(
     batch_size: int,
     sequence_len: int,
     key: jax.Array,
+    bos_token: int | None = None,
+    eos_token: int | None = None,
 ) -> tuple[jax.Array, jax.Array, jax.Array]:
     """Generate a batch of data."""
     batch_keys = jax.random.split(key, batch_size)
     gen_states, obs = data_generator.generate(gen_states, batch_keys, sequence_len, False)
+    if bos_token is not None:
+        obs = jnp.concatenate([jnp.full((batch_size, 1), bos_token), obs], axis=1)
+    if eos_token is not None:
+        obs = jnp.concatenate([obs, jnp.full((batch_size, 1), eos_token)], axis=1)
     inputs = obs[:, :-1]
     labels = obs[:, 1:]
     return gen_states, inputs, labels
