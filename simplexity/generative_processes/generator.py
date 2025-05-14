a501bb6aa8fb449f58fd70f9faae018f74dd0f2a
Eric Alt <ericallenalt@gmail.com>
2025-05-14 21:05:24 +0000

    rename obs -> tokens

--- simplexity/generative_processes/generator.py
+++ simplexity/generative_processes/generator.py
@@ -17,11 +17,11 @@ def generate_data_batch(
 ) -> tuple[jax.Array, jax.Array, jax.Array]:
     """Generate a batch of data."""
     batch_keys = jax.random.split(key, batch_size)
-    gen_states, obs = data_generator.generate(gen_states, batch_keys, sequence_len, False)
+    gen_states, tokens = data_generator.generate(gen_states, batch_keys, sequence_len, False)
     if bos_token is not None:
-        obs = jnp.concatenate([jnp.full((batch_size, 1), bos_token), obs], axis=1)
+        tokens = jnp.concatenate([jnp.full((batch_size, 1), bos_token), tokens], axis=1)
     if eos_token is not None:
-        obs = jnp.concatenate([obs, jnp.full((batch_size, 1), eos_token)], axis=1)
-    inputs = obs[:, :-1]
-    labels = obs[:, 1:]
+        tokens = jnp.concatenate([tokens, jnp.full((batch_size, 1), eos_token)], axis=1)
+    inputs = tokens[:, :-1]
+    labels = tokens[:, 1:]
     return gen_states, inputs, labels
