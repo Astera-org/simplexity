9887b7b2bd89cbc9c20e105a98b7580b25fdeb6b
Eric Alt <ericallenalt@gmail.com>
2025-10-23 13:57:31 +0000

    Update managed_run_demo to log PyTorch models with MLflow when using mlflow_persister

--- examples/managed_run_demo.py
+++ examples/managed_run_demo.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 
 import hydra
-from omegaconf import DictConfig
+import mlflow.pytorch as mlflow_pytorch
+from torch.nn import Module as PytorchModel
 
 import simplexity
+from examples.configs.demo_config import Config
 from simplexity.run_management.run_management import Components
 
 
 @hydra.main(config_path=str(Path(__file__).parent / "configs"), config_name="demo_config.yaml", version_base="1.2")
 @simplexity.managed_run(strict=False, verbose=True)
-def main(cfg: DictConfig, components: Components) -> None:
+def main(cfg: Config, components: Components) -> None:
     """Test the managed run decorator."""
     if components.logger:
         print(f"Logger: {components.logger.__class__.__name__}")
@@ -26,6 +28,18 @@ def main(cfg: DictConfig, components: Components) -> None:
         print("No persister found")
     if components.predictive_model:
         print(f"Predictive model: {components.predictive_model.__class__.__name__}")
+        is_mlflow_persister = cfg.persistence.name == "mlflow_persister"
+        if is_mlflow_persister:
+            instance_config = getattr(cfg.persistence, "instance", None)
+            if instance_config:
+                registered_model_name = getattr(instance_config, "registered_model_name", None)
+                is_pytorch_model = isinstance(components.predictive_model, PytorchModel)
+                if is_pytorch_model and registered_model_name:
+                    mlflow_pytorch.log_model(
+                        components.predictive_model,
+                        name="demo",
+                        registered_model_name=registered_model_name,
+                    )
     else:
         print("No predictive model found")
 
