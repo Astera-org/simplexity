45824a777b6325f0c70f5edd305b69e23b61f65a
Eric Alt <ericallenalt@gmail.com>
2025-10-24 18:27:16 +0000

    Create HookedTransformer structured config

--- simplexity/configs/predictive_model/config.py
+++ simplexity/configs/predictive_model/config.py
@@ -1,25 +1,50 @@
 from dataclasses import dataclass
 from typing import Literal
 
-TARGETS = Literal["simplexity.predictive_models.gru_rnn.build_gru_rnn"]
+from omegaconf import MISSING
 
 
 @dataclass
 class ModelInstanceConfig:
     """Configuration for the model instance."""
 
-    _target_: TARGETS
-    vocab_size: int
-
 
 @dataclass
 class GRURNNConfig(ModelInstanceConfig):
     """Configuration for GRU RNN model."""
 
+    _target_: Literal["simplexity.predictive_models.gru_rnn.build_gru_rnn"]
     embedding_size: int
     num_layers: int
     hidden_size: int
     seed: int
+    vocab_size: int = MISSING
+
+
+@dataclass
+class HookedTransformerConfigConfig:
+    """Configuration for HookedTransformerConfig."""
+
+    _target_: Literal["transformer_lens.HookedTransformerConfig"]
+    d_model: int
+    d_head: int
+    n_heads: int
+    n_layers: int
+    n_ctx: int
+    d_mlp: int
+    act_fn: str | None
+    normalization_type: str | None
+    device: str | None
+    seed: int
+    d_vocab: int = MISSING
+
+
+@dataclass
+class HookedTransformerConfig(ModelInstanceConfig):
+    """Configuration for Transformer model."""
+
+    _target_: Literal["transformer_lens.HookedTransformer"]
+    cfg: HookedTransformerConfigConfig
 
 
 @dataclass
