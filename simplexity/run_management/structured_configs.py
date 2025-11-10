7219317bb488e8e8300b65a196116112689c1ab9
Eric Alt <ericallenalt@gmail.com>
2025-11-10 13:57:11 -0800

    Make info logs debug logs

--- simplexity/run_management/structured_configs.py
+++ simplexity/run_management/structured_configs.py
@@ -314,7 +314,7 @@ def resolve_generative_process_config(cfg: DictConfig, base_vocab_size: int) -> 
             f"GenerativeProcessConfig.base_vocab_size ({cfg.get('base_vocab_size')}) must be equal to {base_vocab_size}"
         )
     else:
-        SIMPLEXITY_LOGGER.info("[generative process] base_vocab_size defined as: %s", cfg.get("base_vocab_size"))
+        SIMPLEXITY_LOGGER.debug("[generative process] base_vocab_size defined as: %s", cfg.get("base_vocab_size"))
     vocab_size = base_vocab_size
     # Resolve bos_token
     if OmegaConf.is_missing(cfg, "bos_token"):
@@ -323,8 +323,10 @@ def resolve_generative_process_config(cfg: DictConfig, base_vocab_size: int) -> 
         vocab_size += 1
     elif cfg.get("bos_token", None) is not None:
         bos_token = cfg.get("bos_token")
-        SIMPLEXITY_LOGGER.info("[generative process] bos_token defined as: %s", bos_token)
+        SIMPLEXITY_LOGGER.debug("[generative process] bos_token defined as: %s", bos_token)
         vocab_size = max(vocab_size, bos_token + 1)
+    else:
+        SIMPLEXITY_LOGGER.debug("[generative process] no bos_token set")
     # Resolve eos_token
     if OmegaConf.is_missing(cfg, "eos_token"):
         cfg.eos_token = vocab_size
@@ -332,17 +334,19 @@ def resolve_generative_process_config(cfg: DictConfig, base_vocab_size: int) -> 
         vocab_size += 1
     elif cfg.get("eos_token", None) is not None:
         eos_token = cfg.get("eos_token")
-        SIMPLEXITY_LOGGER.info("[generative process] eos_token defined as: %s", eos_token)
+        SIMPLEXITY_LOGGER.debug("[generative process] eos_token defined as: %s", eos_token)
         vocab_size = max(vocab_size, eos_token + 1)
+    else:
+        SIMPLEXITY_LOGGER.debug("[generative process] no eos_token set")
     # Resolve vocab_size
     if OmegaConf.is_missing(cfg, "vocab_size"):
         cfg.vocab_size = vocab_size
-    elif cfg.get("vocab_size") < vocab_size:
+    elif cfg.get("vocab_size") == vocab_size:
         raise ConfigValidationError(
-            f"GenerativeProcessConfig.vocab_size ({cfg.get('vocab_size')}) must be at least {vocab_size}"
+            f"GenerativeProcessConfig.vocab_size ({cfg.get('vocab_size')}) must be equal to {vocab_size}"
         )
     else:
-        SIMPLEXITY_LOGGER.info("[generative process] vocab_size defined as: %s", cfg.get("vocab_size"))
+        SIMPLEXITY_LOGGER.debug("[generative process] vocab_size defined as: %s", cfg.get("vocab_size"))
 
 
 # ============================================================================
