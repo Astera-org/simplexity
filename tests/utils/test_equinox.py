1344a437e0d855604479c8099903cfde40f78a1e
Eric Alt <ericallenalt@gmail.com>
2025-11-10 16:54:40 -0800

    Remove equinox gru rnn code

--- tests/utils/test_equinox.py
+++ tests/utils/test_equinox.py
@@ -1,13 +1,12 @@
 import equinox as eqx
 import jax
 
-from simplexity.predictive_models.gru_rnn import build_gru_rnn
 from simplexity.utils.equinox import vmap_model
 
 
 def test_vmap_model():
     vocab_size = 2
-    model = build_gru_rnn(vocab_size=vocab_size, embedding_size=2, num_layers=1, hidden_size=2, seed=0)
+    model = eqx.nn.Linear(in_features=4, out_features=2, key=jax.random.key(0))
 
     def f(model: eqx.Module, inputs: jax.Array) -> jax.Array:
         return model(inputs)
