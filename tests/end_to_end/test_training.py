b70b4d95da5e1153c4357c953ac3dc72def8c8d8
Eric Alt <ericallenalt@gmail.com>
2025-11-29 17:46:18 -0800

    Add checks to training test

--- tests/end_to_end/test_training.py
+++ tests/end_to_end/test_training.py
@@ -11,7 +11,10 @@
 
 from pathlib import Path
 
+import mlflow
+import numpy as np
 from hydra import compose, initialize_config_dir
+from omegaconf import OmegaConf
 
 from tests.end_to_end.training import CONFIG_DIR, CONFIG_NAME, train
 
@@ -19,11 +22,61 @@ from tests.end_to_end.training import CONFIG_DIR, CONFIG_NAME, train
 def test_training(tmp_path: Path) -> None:
     """Test training."""
     with initialize_config_dir(CONFIG_DIR, version_base="1.2"):
-        mlflow_dir = tmp_path / "mlruns"
-        mlflow_uri = f"file:{mlflow_dir.absolute()}"
+        mlflow_db = tmp_path / "mlflow.db"
+        mlflow_uri = f"sqlite:///{mlflow_db.absolute()}"
         overrides = [
             f"mlflow.tracking_uri={mlflow_uri}",
             f"mlflow.registry_uri={mlflow_uri}",
         ]
         cfg = compose(config_name=CONFIG_NAME, overrides=overrides)
     train(cfg)
+
+    client = mlflow.MlflowClient(tracking_uri=mlflow_uri)
+    experiment = client.get_experiment_by_name(cfg.mlflow.experiment_name)
+    assert experiment is not None
+    runs = client.search_runs(experiment_ids=[experiment.experiment_id], max_results=1)
+    assert len(runs) == 1
+    run = runs[0]
+    run_id = run.info.run_id
+
+    # Tags
+    tags = run.data.tags
+    for key, value in cfg.tags.items():
+        assert tags[key] == value
+    assert "strict" in tags
+    for key in ["commit", "commit_full", "dirty", "branch", "remote"]:
+        assert f"git.main.{key}" in tags
+
+    # Parameters
+    parameters = run.data.params
+    assert len(parameters) > 0
+
+    # Config
+    config_path = client.download_artifacts(
+        run_id,
+        path="config.yaml",
+        dst_path=str(tmp_path / "config.yaml"),
+    )
+    assert OmegaConf.load(config_path) == cfg
+
+    # Metrics
+    def get_metric_values(metric_name: str) -> np.ndarray:
+        metric_history = client.get_metric_history(run.info.run_id, metric_name)
+        return np.array([metric.value for metric in metric_history])
+
+    train_loss = get_metric_values("train/loss")
+    eval_loss = get_metric_values("eval/loss")
+    assert train_loss.shape == (cfg.training.num_steps // cfg.training.log_every + 1,)
+    assert eval_loss.shape == (cfg.training.num_steps // cfg.training.evaluate_every + 1,)
+    assert np.all(train_loss > 0)
+    assert np.all(eval_loss > 0)
+
+    # Checkpoints
+    model_dir = cfg.persistence.instance.model_dir or "models"
+    checkpoints = client.list_artifacts(run.info.run_id, model_dir)
+    assert len(checkpoints) == cfg.training.num_steps // cfg.training.checkpoint_every + 1
+
+    # Logged models
+    registered_model_name = cfg.predictive_model.name or "test_model"
+    model = client.get_registered_model(registered_model_name)
+    assert model is not None
