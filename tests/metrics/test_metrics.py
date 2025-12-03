d6e2cabd22271903da5d7a29840ddfa7886be262
Eric Alt <ericallenalt@gmail.com>
2025-12-03 15:51:19 -0800

    learning rates metric test

--- tests/metrics/test_metrics.py
+++ tests/metrics/test_metrics.py
@@ -1,6 +1,13 @@
 """Tests for the metrics module."""
 
-from simplexity.metrics.metrics import RequiredFields, Requirements, combine_required_fields, combine_requirements
+from simplexity.metrics.metrics import (
+    Context,
+    LearningRateMetric,
+    RequiredFields,
+    Requirements,
+    combine_required_fields,
+    combine_requirements,
+)
 
 
 def test_required_fields():
@@ -43,3 +50,17 @@ def test_combine_requirements():
     assert combined_requirements.init.learning_rates
     assert combined_requirements.step.gradients
     assert combined_requirements.compute.named_parameters
+
+
+def test_learning_rates():
+    """Test the LearningRates class."""
+    context = Context()
+    metric = LearningRateMetric(context)
+    metric.step(context)
+    assert metric.compute(context) == {}
+
+    context = Context(learning_rates={"lr": 0.01})
+    assert metric.compute(context) == {"step/learning_rate": 0.01}
+
+    context = Context(learning_rates={"lr1": 0.01, "lr2": 0.02})
+    assert metric.compute(context) == {"learning_rate/lr1": 0.01, "learning_rate/lr2": 0.02}
