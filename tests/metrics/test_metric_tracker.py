7ac18cb60ee1d7fe1d589c39de74070378ebfb1f
Eric Alt <ericallenalt@gmail.com>
2025-12-04 16:35:07 -0800

    Refactor tests

--- tests/metrics/test_metric_tracker.py
+++ tests/metrics/test_metric_tracker.py
@@ -31,9 +31,6 @@ def test_init_all_metrics(model: torch.nn.Module, optimizer: torch.optim.Optimiz
     """Test the MetricTracker class."""
     metric_tracker = MetricTracker(model=model, optimizer=optimizer)
     assert set(metric_tracker.metric_groups["all"]) == set(ALL_METRICS.keys())
-    assert metric_tracker.context.learning_rates == {}
-    assert metric_tracker.context.gradients == {}
-    assert metric_tracker.context.named_parameters == {}
 
 
 def test_init_metric_list():
@@ -60,12 +57,14 @@ def test_init_metric_dict(optimizer: torch.optim.Optimizer):
 
 def test_warn_missing_context():
     """Test the MetricTracker class."""
+    assert ALL_METRICS["learning_rate"].requirements.context_field_required("learning_rates")
     with patch("simplexity.metrics.metric_tracker.SIMPLEXITY_LOGGER.warning") as mock_warning:
         MetricTracker(metric_names=["learning_rate"])
         mock_warning.assert_called_once_with(
             "[Metrics] %s requires learning rates, but optimizer is not set in MetricTracker", "learning_rate"
         )
 
+    assert ALL_METRICS["parameter_distance"].requirements.context_field_required("named_parameters")
     with patch("simplexity.metrics.metric_tracker.SIMPLEXITY_LOGGER.warning") as mock_warning:
         MetricTracker(metric_names=["parameter_distance"])
         mock_warning.assert_called_once_with(
@@ -76,25 +75,36 @@ def test_warn_missing_context():
 
 def test_lazy_context_update(model: torch.nn.Module, optimizer: torch.optim.Optimizer):
     """Test the MetricTracker class."""
+    metric_requirements = ALL_METRICS["learning_rate"].requirements
     metric_tracker = MetricTracker(metric_names=["learning_rate"], optimizer=optimizer)
-    assert metric_tracker.context.learning_rates == {}  # learning_rate does not require lr during init
+    assert not metric_requirements.init_required
+    assert metric_tracker.context.learning_rates == {}
     metric_tracker.step()
-    assert metric_tracker.context.learning_rates == {}  # learning_rate does not require lr during step
+    assert not metric_requirements.step_required
+    assert metric_tracker.context.learning_rates == {}
     metric_tracker.get_metrics()
-    assert metric_tracker.context.learning_rates != {}  # learning_rate requires lr during compute
+    assert metric_requirements.compute_required
+    assert metric_tracker.context.learning_rates != {}
 
+    metric_requirements = ALL_METRICS["gradient_weighted_tokens"].requirements
     metric_tracker = MetricTracker(metric_names=["gradient_weighted_tokens"], model=model, optimizer=optimizer)
-    assert metric_tracker.context.gradients == {}  # gradient_weighted_tokens does not require gradients during init
+    assert not metric_requirements.init_required
+    assert metric_tracker.context.gradients == {}
     _induce_gradients(model)
     metric_tracker.step()
-    assert metric_tracker.context.gradients != {}  # gradient_weighted_tokens does requires gradients during step
+    assert metric_requirements.step_required
+    assert metric_tracker.context.gradients != {}
 
+    metric_requirements = ALL_METRICS["parameter_distance"].requirements
     metric_tracker = MetricTracker(metric_names=["parameter_distance"], model=model)
-    assert metric_tracker.context.named_parameters != {}  # parameter_distance requires params during init
+    assert metric_requirements.init_required
+    assert metric_tracker.context.named_parameters != {}
     metric_tracker.step()
-    assert metric_tracker.context.named_parameters == {}  # parameter_distance does not require params during step
+    assert not metric_requirements.step_required
+    assert metric_tracker.context.named_parameters == {}
     metric_tracker.get_metrics()
-    assert metric_tracker.context.named_parameters != {}  # parameter_distance requires params during compute
+    assert metric_requirements.compute_required
+    assert metric_tracker.context.named_parameters != {}
 
 
 def test_cache_computed_metrics():
