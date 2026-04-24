0425632e9c1314ec45da9dda23fa17f5f70c550c
14xp <xponcini@gmail.com>
2026-04-24 16:31:54 +0100

    mess3 order one processes added

--- simplexity/generative_processes/transition_matrices.py
+++ simplexity/generative_processes/transition_matrices.py
@@ -198,6 +198,31 @@ def mess3(x: float, a: float) -> jax.Array:
     )
 
 
+def mess3_order_one(x: float, a: float) -> jax.Array:
+    """Creates the order-one transition matrices for the Mess3 Process."""
+    A = 0.5 * (1 - 2 * a + 3 * a**2 - x + 6 * a * x - 9 * a**2 * x)
+    B = 0.25 * (1 + 2 * a - 3 * a**2 + x - 6 * a * x + 9 * a**2 * x)
+    return jnp.array(
+        [
+            [
+                [A, 0, 0],
+                [B, 0, 0],
+                [B, 0, 0],
+            ],
+            [
+                [0, B, 0],
+                [0, A, 0],
+                [0, B, 0],
+            ],
+            [
+                [0, 0, B],
+                [0, 0, B],
+                [0, 0, A],
+            ],
+        ]
+    )
+
+
 def mr_name(p: float, q: float) -> jax.Array:
     """Creates a transition matrix for the Mr. Dursley/Wonka Process."""
     assert 0 <= p <= 1
@@ -434,6 +459,7 @@ HMM_MATRIX_FUNCTIONS = {
     "leopard": leopard,
     "matching_parens": matching_parens,
     "mess3": mess3,
+    "mess3_order_one": mess3_order_one,
     "mr_name": mr_name,
     "no_consecutive_ones": no_consecutive_ones,
     "rrxor": rrxor,
